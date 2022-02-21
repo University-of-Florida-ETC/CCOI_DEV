1e409f3604bb94bb6cd6b6f291b772c1c7031d83
Zack <zneidig@ufl.edu>
2022-02-21 15:14:22 -0500

    More debug sql stmts

--- api/ccoi_ajax.php
+++ api/ccoi_ajax.php
@@ -576,11 +576,37 @@ if(!empty($_GET['uid2']) && is_numeric($_GET['uid2']) && !isset($_GET['debug']))
 	}
 }
 
+//ZACK'S DEBUG STUFF
+//====================================================================================================================================================
 if(!empty($_GET['uid2']) && is_numeric($_GET['uid2']) && isset($_GET['debug'])){
 	echo "debug output will go here, uid2=".$_GET['uid2'];
 	$uid=$_GET['uid2']+0;
 	if(is_numeric($uid)){
-		$return=mysqli_query($db,"SELECT * FROM tbPeople WHERE id='$uid'");		$persondata=mysqli_fetch_assoc($return); echo "\npersondata: ". var_dump($persondata);
+
+		//Get info on person, doesn't seem to be used for much aside from "oldname" which is set to observer? Is NULL for me, can probably skip
+		$return=mysqli_query($db,"SELECT * FROM tbPeople WHERE id='$uid'");		$persondata=mysqli_fetch_assoc($return); //echo "\npersondata: ". var_dump($persondata);
+
+
+		//Get session IDs of research sessions
+		$return=mysqli_query($db,"SELECT sessionid FROM tbPeopleAppSessions WHERE personid='$uid' AND appid='1' AND inactive IS NULL");		
+		while($d=mysqli_fetch_assoc($return)){$sessionids[]=$d['sessionid'];}
+		$sidstext=implode(',',$sessionids);
+		echo "\nsessionids: "; var_dump($sidstext);
+
+		$return=mysqli_query($db,"SELECT sessionid FROM tbPeopleAppPlaygrounds WHERE personid='$uid' AND appid='1' AND inactive IS NULL");		
+		while($d=mysqli_fetch_assoc($return)){$playids[]=$d['sessionid'];}
+		$playidstext=implode(',',$playids);
+		echo "\nplayids: "; var_dump($playidstext);
+		/*
+		$return=mysqli_query($db,"SELECT s.*, v.url FROM tbSessions s LEFT JOIN tbVideos v ON s.videoid=v.id WHERE s.id IN ($sidstext) AND s.inactive IS NULL");				// ====== NOTE NOTE NOTE if there are no videos, this might return fewer results
+		while($d=mysqli_fetch_assoc($return)){$sessions[$d['id']]['s']=$d;}
+		
+ 				$return=mysqli_query($db,"SELECT s.*, v.url FROM tbPlaygrounds s LEFT JOIN tbVideos v ON s.videoid=v.id WHERE s.id IN ($playidstext) AND s.inactive IS NULL");		// ====== NOTE NOTE NOTE if there are no videos, this might return fewer results
+ 				while($d=mysqli_fetch_assoc($return)){$playgrounds[$d['id']]['s']=$d;}		//print_r($playgrounds);
+
+		$return=mysqli_query($db,"SELECT * FROM tbNodes WHERE 1");		
+		while($d=mysqli_fetch_assoc($return)){$nodeData[$d['id']]=$d;}
+		*/
 	}
 	/*
 	$persondata=mysqli_fetch_assoc($return);
