d30f00a402ba1630c3dd5acffc7b2f84277a6c5a
Zack <zneidig@ufl.edu>
2022-02-25 14:08:28 -0500

    Debug pt3

--- ZPB/observation.php
+++ ZPB/observation.php
@@ -109,9 +109,9 @@ while($d=mysqli_fetch_assoc($return)){
 
                         <div id="path_listing" class="col-12 pt-4 pr-md-5">
                                     <div id="path_list" class="draggable-container">
-<?php foreach ($subsessions as $key=>$currentSub): ?>
+<?php $count = 1; foreach ($subsessions as $key=>$currentSub): ?>
                                         <div class="path-listing-container">
-                                            <h5 data-index="0" class="path-listing-header">Path #<?=$key;?> (ganflgnfa)
+                                            <h5 data-index="0" class="path-listing-header">Path #<?=$count;?> (ganflgnfa)
                                                 <a class="btn-link path-edit-icon" href="#" data-index="0"><span class="oi oi-pencil px-3" title="Edit Path" aria-hidden="true"></span></a>
                                                 <a class="btn-link path-delete-icon" href="#" data-index="0"><span class="oi oi-trash" title="Delete Path" aria-hidden="true"></span></a>
                                                 <button class="btn-link float-right path-dropdown-btn" data-toggle="collapse" data-target="#path_drop_0" aria-expanded="true"><span class="oi oi-chevron-top" title="Show Path Steps" aria-hidden="true"></span></button>
@@ -120,7 +120,7 @@ while($d=mysqli_fetch_assoc($return)){
                                                 <?php var_dump($currentSub); ?>
                                             </p>
                                         </div>
-<?php endforeach; ?>
+<?php $count++; endforeach; ?>
                                         <div class="path-listing-container">
                                             <h5 data-index="0" class="path-listing-header">Path #1 (ganflgnfa)
                                                 <a class="btn-link path-edit-icon" href="#" data-index="0"><span class="oi oi-pencil px-3" title="Edit Path" aria-hidden="true"></span></a>
