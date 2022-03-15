2a7743ed30883a0e20f3946b71de4fc07186edad
Zack <zneidig@ufl.edu>
2022-03-15 15:44:34 -0400

    Attempt actual queries

--- ZPB/zpb_ajax.php
+++ ZPB/zpb_ajax.php
@@ -1,6 +1,7 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/api/ccoi_session.php');
 if( !empty($_POST['newSession']) ) {
+    /*
     $_POST['pathid'] = 1;
     $_POST['playground'] = 1;
 
@@ -10,9 +11,9 @@ if( !empty($_POST['newSession']) ) {
     echo "Query 2: ".$query."\n";
     $query="INSERT INTO tbActivityLog (action, onid, field, details, actby, acton) VALUES ('newobs','{$_POST['pathid']}','new','{$lastid}','{$_SESSION['pid']}',NOW())";
     echo "Query 3: ".$query."\n";
+    */
 
-
-    /*
+    
     $requiredValues = ['name'];     //TODO: pathID and research/playground must be specified, currently no interface for that on front-end
 
     // TEMPORARY, REMOVE LATER
@@ -30,7 +31,7 @@ if( !empty($_POST['newSession']) ) {
 
     
 
-    $query="INSERT INTO tbPlaygrounds (pathid,name,createdon) VALUES ('{$_POST['pathid']}','{$name}',NOW())";			// === NOTE == TODO == this path should not be set here, but in response to data sent in the request
+    $query="INSERT INTO tbPlaygrounds (pathid,name,createdon) VALUES ('{$_POST['pathid']}','{$_POST['name']}',NOW())";
         $return=mysqli_query($db,$query);
         $lastid=mysqli_insert_id($db);
         $returnData['id'] = $lastid;
@@ -43,6 +44,6 @@ if( !empty($_POST['newSession']) ) {
         $return=mysqli_query($db,$query);
 
     echo $returnData['id'];
-    */
+    
 }
 ?>
