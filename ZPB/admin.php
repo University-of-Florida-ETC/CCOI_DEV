cb028dd01ab61148a1ec3ff91b0ba1b5b0e4f6be
Zack <zneidig@ufl.edu>
2022-03-22 16:35:50 -0400

    visual change

--- ZPB/admin.php
+++ ZPB/admin.php
@@ -25,7 +25,7 @@ $users = getUsers();
                         <div class="col-md-12">
                         <div class="row">
                                 <div class="col-md-8 col-12">
-                                    <h1 class="red-font">User Managements</h1>
+                                    <h1 class="red-font">User Management</h1>
                                 </div>
                                 <div class="col-md-4 col-12 pt-2">
                                     <button id="new_user_button" type="button" class="btn btn-gold float-right" data-toggle="tooltip" data-html="true" title="Click here to start">Create New User</button>
