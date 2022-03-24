6639f0b0c2344c374f37fb33c18f2600e2a1658a
Zack <zneidig@ufl.edu>
2022-03-24 09:20:54 -0400

    skip calltoapi

--- js/observe.js
+++ js/observe.js
@@ -2,14 +2,10 @@
 
 $(function() {
     if (typeof jsonReplacement !== 'undefined') {
-        ccoi.callToAPI(jsonReplacement).then(function(result) {
-            ccoi.ccoiSchema = ccoi.parseCCOI(result);
-        }).then(function(){
-            setUpObservation();
-            $('#coder_name').text(jsUserVars['first'] + " " + jsUserVars['last'] + "'s Sessions");
-        }, function(error) {
-            console.log(error)
-        })
+        result = jsonReplacement;
+        ccoi.ccoiSchema = ccoi.parseCCOI(result);
+        setUpObservation();
+        $('#coder_name').text(jsUserVars['first'] + " " + jsUserVars['last'] + "'s Sessions");
     
         function setUpObservation() {
             ccoiObservation.setDemoBool(false);
