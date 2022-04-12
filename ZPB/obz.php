efe4d24b93b41508fa1b7dd2d23dd353122e43c7
Zack <zneidig@ufl.edu>
2022-04-12 15:49:44 -0400

    gottem

--- ZPB/obz.php
+++ ZPB/obz.php
@@ -51,7 +51,7 @@ while ($d = mysqli_fetch_assoc($return)) {
 <script>
     var sessionID = <?php echo $id; ?>;
     var nodeData = `<?php echo json_encode($nodeData, JSON_PRETTY_PRINT); ?>`;
-    var structure = `<?php echo json_encode($structure, JSON_PRETTY_PRINT); ?>`;
+    var structure = JSON.parse(`<?php echo json_encode($structure); ?>`);
 </script>
 <main role="main">
     <div class="container-fluid">
@@ -66,7 +66,7 @@ while ($d = mysqli_fetch_assoc($return)) {
                     <div class="row pr-md-5">
                         <div class="col-md-8 col-12">
                             <h1 class="red-font" id="sessionTitle"><?php echo $session['name']; ?></h1>
-                            <h5 style="text-transform: none;">Select an observation to view or edit its responses</h5>
+                            <h5 style="text-transform: none;">Select an observation to view or edit its responsess</h5>
                         </div>
                         <div class="col-md-4 col-12 pt-2">
                             <button id="save_session_button" type="button" class="btn btn-blue float-right disabled" data-toggle="tooltip" data-html="true" title="Click here to save your session">Save Session</button>
