bfdbf30ea2082a2d5f37915f65eb11fe635ac3dc
Brandon Clark <bwclark97@ufl.edu>
2022-04-21 09:21:50 -0400

    Remove playground printz

--- ZPB/obz.php
+++ ZPB/obz.php
@@ -662,7 +662,7 @@ function getAppVideos($id)
         if (!empty($_GET['isPlayground'])) {
             $return = mysqli_query($db, "SELECT v.id, v.name, v.url FROM tbVideos v LEFT JOIN tbPeopleAppPlaygrounds pg ON pg.appid = v.appid WHERE pg.id = 1 AND pg.inactive IS NULL");
             while ($d = mysqli_fetch_assoc($return)) {
-                print_r($d);
+                //print_r($d);
                 $appVideos = $d;
             }
         } else {
