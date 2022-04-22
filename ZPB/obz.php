26d80b118fedfaf6471b059aef405b30316d3724
Zack <zneidig@ufl.edu>
2022-04-22 13:27:06 -0400

    fix conditional

--- ZPB/obz.php
+++ ZPB/obz.php
@@ -50,7 +50,7 @@ while ($d = mysqli_fetch_assoc($return)) {
 <script>
     var sessionID = <?php echo $id; ?>;
     var subsessions = <?php echo json_encode($subsessions); ?>;
-    if (subsessions = null) {
+    if (subsessions == null) {
         subsessions = {};
     }
     console.log("subsessions:"); console.log(subsessions);
