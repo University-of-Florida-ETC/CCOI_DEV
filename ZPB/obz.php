3264e4265cf189564caca909637ed3468a10352a
Brandon Clark <bwclark97@ufl.edu>
2022-04-28 10:48:52 -0400

    TEST

--- ZPB/obz.php
+++ ZPB/obz.php
@@ -305,7 +305,7 @@ while ($d = mysqli_fetch_assoc($return)) {
             $.ajax({
                 url: '/ZPB/zpb_ajax.php?updateMeta=1&id=' + sessionID,
                 type: 'POST',
-                data: JSON.stringify(sessionMeta),
+                data: sessionMeta,
                 //contentType: 'application/json; charset=utf-8',
                 //dataType: 'json',
                 async: false,
