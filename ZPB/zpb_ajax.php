d58c55adec4448cc22de0db89efd07b358fbd45c
Brandon Clark <bwclark97@ufl.edu>
2022-04-28 10:09:31 -0400

    here

--- ZPB/zpb_ajax.php
+++ ZPB/zpb_ajax.php
@@ -372,7 +372,7 @@ if( !empty($_GET['updateMeta']) ) {
 
     $query="UPDATE tb{$tbName}s SET name='".$_POST['name']."', studentid = {$_POST['studentid']}, placetime = '".$_POST['placetime']."', notes = '".$_POST['notes']."' WHERE id = {$_GET['id']}";
     $return=mysqli_query($db,$query);
-    echo "UPDATE tb{$tbName}s SET name='{$_POST['name']}', studentid = {$_POST['studentid']}, placetime = '{$_POST['placetime']}', notes = '{$_POST['notes']}' WHERE id = {$_GET['id']}";
+    echo $query;
     echo $_GET['id'];
 
 }
