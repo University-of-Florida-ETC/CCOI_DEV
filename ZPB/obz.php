bd31b405b0d44f04c84e124ea2168a5106174067
Brandon Clark <bwclark97@ufl.edu>
2022-04-28 10:14:20 -0400

    Try this out I guess

--- ZPB/obz.php
+++ ZPB/obz.php
@@ -304,7 +304,7 @@ while ($d = mysqli_fetch_assoc($return)) {
             $.ajax({
                 url: '/ZPB/zpb_ajax.php?updateMeta=1&id=' + sessionID,
                 type: 'POST',
-                data: sessionMeta,
+                data: JSON.stringify(sessionMeta),
                 contentType: 'application/json; charset=utf-8',
                 dataType: 'json',
                 async: false,
