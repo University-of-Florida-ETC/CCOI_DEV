7a9dd090f45eb455407097a190f6000e79a34508
Brandon Clark <bwclark97@ufl.edu>
2022-04-29 13:10:06 -0400

    Thank god for the waters waters... thank god.

--- ZPB/zpb_ajax.php
+++ ZPB/zpb_ajax.php
@@ -5,7 +5,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/api/ccoi_dbhookup.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     var_dump($_POST);
 }*/
-
+//TODO Scrub $_POST and $_GET with Mark's one liner and start pulling from $_SCRUBEBD within queries.
 if( !empty($_POST['newSession']) ) {
 
     $possibleValues = ['name', 'studentid', 'date', 'video', 'path'];     //TODO: pathID and research/playground must be specified, currently no interface for that on front-end
