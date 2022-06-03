9a62d419468457ff3cd38947b11edad780605951
Brandon Clark <bwclark97@ufl.edu>
2022-06-03 15:51:48 -0400

    This is absolutely poggers

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <?php
 //CCOI header file
 
-if (substr_count($_SERVER[‘HTTP_ACCEPT_ENCODING’], ‘gzip’))
-    ob_start(“ob_gzhandler”);
+if (substr_count($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip"))
+    ob_start("ob_gzhandler");
 else ob_start();
 ?>
 <!DOCTYPE html>
